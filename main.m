4337ad54fc500bce5568fd6fcacaa90b6cacf9f9
ellaHerzberg <321ella123@gmail.com>
2022-06-13 23:29:01 +0300

    fix plots ans ICA sunction, need to finish sanger's part

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ clc;
 mpath = strrep(which(mfilename),[mfilename '.m'],'');
 addpath([mpath 'ActivationFunctions']);
 
-%% a + b. using informax ICA to separate noiseless mix
+%% a. + b. using informax ICA to separate noiseless mix
 % Parameters and initialization
 nDimentions=3;
 nEpochs=10;
@@ -34,29 +34,67 @@ end
 
 
 %% ICA
-separate_on = noiseless;
+%randomly choose 3 channels for noisy mix
+noisy_to_separated = noisy(:,randperm(7,3)); 
 
-% separate_on = noisy(:,randperm(7,3)); %randomly choose 3 channels
-sampleSize=size(separate_on,1);
-for epoch=1:nEpochs
-    randOrder = randperm(sampleSize);
-    for i=1:sampleSize
-        xi=separate_on(randOrder(i),:)';
-        g=activeFunction(W*xi);
-        y=1-2*g;
-        dw=eta*(inv(W')+y*xi'); %learning rule of weights
-        W=W+dw; %update weights
-    end
-    eta=0.1/(1+1e-4*epoch); %update eta each epoch
-end
-% Output Seperated Sources
-separated=(W*separate_on')';
+% Seperated Sources for noisy and noiseless with ICA
+[noiseless_separated, ~] =ICA(noiseless, W, nEpochs, eta, activeFunction);
+[noisy_separated, W_noisy] =ICA(noisy_to_separated, W, nEpochs, eta, activeFunction);
 
 
 %% plot
+separated_mixes{1} = noiseless_separated;
+separated_mixes{2} = noisy_separated;
+
 channel_N = size(sources, 2);
 
+for i = 1:2
+    separated = separated_mixes{i};
+    % Compute correlation with the source and seperated data
+    correlation_mat = abs(corr(separated, sources));
+    max_corr = max(correlation_mat); %extract max correlations
+
+    % fix shifting
+    max_indexes = find(correlation_mat == max_corr)- [0;3;6]; 
+    separated = separated(:,max_indexes');
+
+    % Plot
+    figure
+    main_title = 'Corrolation between source and seperated noiseless data';
+    if i==2
+        main_title = 'Corrolation between source and seperated noisy data';
+    end
+    
+    sgtitle(main_title)
+    for j = 1 : channel_N
+        subplot(2, channel_N, j);
+        plot(sources(:,j));
+        title(['Source ',num2str(j)]);
+        subplot(2,channel_N ,j + channel_N);
+        plot(separated(:,j));
+        title(['Seperated' ,num2str(j), newline,'Cor - ' num2str(max_corr(j))]); 
+    end
+end
+
+%% c. Sanger's rule
+
+% Compute eigenvectors using Sanger's rule
+Sanger_eigenvectors = Sanger(noisy, eta);
+% Compute eigenvectors using matlab's PCA function
+pca_eigenvectors = pca(noisy);
+% Compare the distance between both sets of eigenvectors
+distance = sqrt(sum((Sanger_eigenvectors-pca_eigenvectors).^2));
+
+% Reduce the noisy data to 3 dimensions using sanger's weights
+noisy_lower_dim = noisy*Sanger_eigenvectors(:,1:nDimentions);
+
+% Seperated Sources for with ICA
+[noisy_lower_dim_separated, W_noisy_lower_dim] =...
+    ICA(noisy_lower_dim, W, nEpochs, eta, activeFunction);
+
+%% plot
 % Compute correlation with the source and seperated data
+separated = noisy_lower_dim_separated;
 correlation_mat = abs(corr(separated, sources));
 max_corr = max(correlation_mat); %extract max correlations
 
@@ -66,12 +104,14 @@ separated = separated(:,max_indexes');
 
 % Plot
 figure
-sgtitle('Corrolation between source and seperated data')
-for i = 1 : channel_N
-    subplot(2, channel_N, i);
-    plot(sources(:,i));
-    title(['Source ',num2str(i)]);
-    subplot(2,channel_N ,i + channel_N);
-    plot(separated(:,i));
-    title(['Seperated' ,num2str(i), newline,'Cor - ' num2str(max_corr(i))]); 
+main_title = 'Corrolation between source and seperated lower dim data';
+
+sgtitle(main_title)
+for j = 1 : channel_N
+    subplot(2, channel_N, j);
+    plot(sources(:,j));
+    title(['Source ',num2str(j)]);
+    subplot(2,channel_N ,j + channel_N);
+    plot(separated(:,j));
+    title(['Seperated' ,num2str(j), newline,'Cor - ' num2str(max_corr(j))]); 
 end
